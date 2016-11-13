5e0f161fb073b4759c70cb6ef7c9b47d523a9160
Eleni Litsa <litsaelen@gmail.com>
2016-11-13 14:20:04 -0600

    it runs

--- project5.py
+++ project5.py
@@ -9,7 +9,7 @@ from Scientific.Visualization import VMD; module = VMD
 
 # Load the PDB file.
 #configuration = PDBConfiguration('1nxb.pdb')
-configuration = PDBConfiguration('110d.pdb')
+#configuration = PDBConfiguration('110d.pdb')
 configuration = PDBConfiguration('insulin.pdb')
 
 # Construct the peptide chain objects.
@@ -28,7 +28,6 @@ numberOfResidues = len(chain)
 # Access all residues
 #for residue in chain: do sth
 
-# Keep the substructure formed by the residues 23–42
 # p[i:j] yields the subchain of chain p from residue number i up to but excluding residue number j
 #sc = chain[23:43]
 
@@ -39,8 +38,8 @@ r1 = chain[1]
 angle = r1.chiAngle()
 
 # Create a universe and put the chain in it
-universe = InfiniteUniverse(Amber99ForceField())
-universe.addObject(sc)
+universe = InfiniteUniverse(Amber94ForceField())
+universe.addObject(chain)
 # Current configuration of universe
 universe.configuration()
 # List of atoms in universe
