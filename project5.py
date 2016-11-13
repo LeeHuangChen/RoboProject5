5c333e0c3d37fc1899f9685c94b9530c3455047f
Eleni Litsa <litsaelen@gmail.com>
2016-11-13 14:07:42 -0600

    access residues

--- project5.py
+++ project5.py
@@ -1,6 +1,7 @@
 from MMTK import *
 from MMTK.PDB import PDBConfiguration
 from MMTK.Proteins import PeptideChain
+from MMTK.Proteins import Protein
 from MMTK.Collections import Collection
 from MMTK.ForceFields import Amber94ForceField
 #from MMTK.Visualization import view
@@ -9,15 +10,17 @@ from Scientific.Visualization import VMD; module = VMD
 # Load the PDB file.
 #configuration = PDBConfiguration('1nxb.pdb')
 configuration = PDBConfiguration('110d.pdb')
+configuration = PDBConfiguration('insulin.pdb')
 
 # Construct the peptide chain objects.
-#chains = configuration.createPeptideChains()
+chains = configuration.createPeptideChains()
 
 # Make the protein object.
-#protein = Protein(chains)
+protein = Protein(chains)
 
 # The protein contains only one chain
-chain = configuration.peptide_chains[0]
+#chain = configuration.peptide_chains[0]
+chain = chains[0]
 
 # Number of residues in Protein
 numberOfResidues = len(chain)
@@ -27,10 +30,15 @@ numberOfResidues = len(chain)
 
 # Keep the substructure formed by the residues 23–42
 # p[i:j] yields the subchain of chain p from residue number i up to but excluding residue number j
-sc = chain[23:43]
+#sc = chain[23:43]
+
+# access the i-th residue
+r1 = chain[1]
+
+# DihedralAngle 
+angle = r1.chiAngle()
 
 # Create a universe and put the chain in it
-#universe = InfiniteUniverse()
 universe = InfiniteUniverse(Amber99ForceField())
 universe.addObject(sc)
 # Current configuration of universe
